022a73fa6db431977422182557097624da1e9a80
YungNewton <isaacnewtonahanmisi@gmail.com>
2023-06-18 17:09:18 +0100

    commit

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 app.set('view engine', 'ejs');
+const homeContent = "This is the home page";
+const contactContent = "Contact me at newton Dev"
+const about = "I am newton";
 
 app.get('/', (req, res)=>{
-    res.send("<h1>Home<h1/>");
+    res.render('index',{home: homeContent, aboutMe: about, contact: contactContent});
 })
 
 app.listen(port, ()=>{
